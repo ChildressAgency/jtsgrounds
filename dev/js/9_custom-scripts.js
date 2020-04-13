4064043128dab7ffb31915ab26b78778c91a2806
childressdev <jcampbell@childressagency.com>
2020-04-13 17:29:47 -0400

    odometer animation

--- dev/js/9_custom-scripts.js
+++ dev/js/9_custom-scripts.js
@@ -19,13 +19,43 @@ jQuery(document).ready(function($){
     dash: {len:8, gap:6}
   };
 
-  new LeaderLine(bubble1, bubble2, lineOptions);
-  new LeaderLine(bubble2, bubble3, lineOptions);
-  new LeaderLine(bubble3, bubble4, lineOptions);
-  new LeaderLine(bubble1, bubble5, lineOptions);
+  if((typeof(bubble1) !== 'undefined' && bubble1 !== null)
+    || (typeof (bubble2) !== 'undefined' && bubble2 !== null)
+    || (typeof (bubble3) !== 'undefined' && bubble3 !== null)
+    || (typeof (bubble4) !== 'undefined' && bubble4 !== null)
+    || (typeof (bubble5) !== 'undefined' && bubble5 !== null)){
+    new LeaderLine(bubble1, bubble2, lineOptions);
+    new LeaderLine(bubble2, bubble3, lineOptions);
+    new LeaderLine(bubble3, bubble4, lineOptions);
+    new LeaderLine(bubble1, bubble5, lineOptions);
+  }
 
   $('.grid').masonry({
     itemSelector: '.grid-item',
     columnWidth: 200
-  })
+  });
+
+  $(window).on('scroll', function(){
+    if(this.isInViewport($('.stats'), 100)){
+      $('.stat-number').each(function(index){
+        var topNumber = $(this).data('top_number');
+        $(this).text(topNumber);
+      });
+    }
+  });
 }); //end jquery
+
+function isInViewport(element, offset){
+  if(element.length == 0){ return; }
+
+  var $window = $(window);
+  var viewportTop = $window.scrollTop();
+  var viewportHeight = $window.height();
+  var viewportBottom = viewportTop + viewportHeight - offset;
+  var $element = $(element);
+  var top = $element.offset().top;
+  var height = $element.height();
+  var bottom = top + height;
+
+  return (bottom > viewportTop) && (top < viewportBottom);
+}
