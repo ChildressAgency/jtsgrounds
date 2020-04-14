edf4b4e8f55924f3225e5d951b3802c45fbe361e
childressdev <jcampbell@childressagency.com>
2020-04-14 19:39:14 -0400

    gallery section styled

--- dev/js/9_custom-scripts.js
+++ dev/js/9_custom-scripts.js
@@ -30,9 +30,44 @@ jQuery(document).ready(function($){
     new LeaderLine(bubble1, bubble5, lineOptions);
   }
 
-  $('.grid').masonry({
+  var $masonryGrid = $('.grid').masonry({
     itemSelector: '.grid-item',
-    columnWidth: 200
+    //columnWidth: 200
+  });
+
+  /*$masonryGrid.on('click', 'img', function(event){
+    $('.grid-item').not(this.parent()).each(function(){
+      $(this).removeClass('grid-item--expanded');
+    });
+    $(event.currentTarget).parent('.grid-item').toggleClass('grid-item--expanded');
+
+    $masonryGrid.masonry();
+  });*/
+
+  $masonryGrid.on('click', '.grid-item', function(e){
+    $('.grid-item').not(this).each(function(){
+      $(this).removeClass('grid-item--expanded');
+    });
+
+    $(e.currentTarget).toggleClass('grid-item--expanded');
+
+    $masonryGrid.masonry();
+  });
+
+  $('.gallery-item').on('click', function(e){
+    e.preventDefault();
+  });
+
+  $('#gallery-modal').on('show.bs.modal', function(e){
+    var galleryItem = $(e.relatedTarget);
+    var jtsFullSize = galleryItem.data('jts_full_size');
+    var jtsJobTitle = galleryItem.data('jts_job_title');
+    //var jtsJobSite = galleryItem.data('jts_job_site');
+    var modal = $(this);
+
+    modal.find('#job-title').text(jtsJobTitle);
+    //modal.find('#job-site').text(jtsJobSite);
+    modal.find('#gallery-image').attr('src', jtsFullSize);
   });
 
   $(window).on('scroll', function(){
